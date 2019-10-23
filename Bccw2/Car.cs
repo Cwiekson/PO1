a03c9023936beb6736d48e3cec356e83ed2e6d01
Patryk98 <patrys986@wp.pl>
2019-10-23 16:11:19 +0200

    Klasy

---
+++ Bccw2/Car.cs
@@ -0,0 +1,29 @@
+﻿namespace Bccw2
+{
+    class Car
+    {
+         public string marka;
+         public int Rokprodukcji;
+        private string model;
+        private int iloscDrzwi;
+        private double pojemnoscSilnika;
+        public double srednieSpalanie;
+
+        public Car(string marka, int rokprodukcji)
+        {
+            this.marka = marka;
+            Rokprodukcji = rokprodukcji;
+        }
+        private double ObliczSpalanie(double dlugoscTrasy)
+        {
+            
+            double spalanie = srednieSpalanie * dlugoscTrasy;
+            return spalanie;
+        }
+        public double ObliczKosztPrzejazdu(double cenaPaliwa, double dlugoscTras)
+        {
+          double  kosztPrzejazdu = srednieSpalanie * cenaPaliwa;
+            return kosztPrzejazdu;
+        }
+    }
+}
--- Bccw2/Program.cs
+++ Bccw2/Program.cs
@@ -10,11 +10,17 @@ namespace Bccw2
     {
         static void Main(string[] args)
         {
+            /*//string carName = "MojSamochod";
+            Car car1=new Car("Fiat",1999);
+            Console.WriteLine("{0} {1} " ,car1.marka ,car1.Rokprodukcji);
+            Car car2 = car1;
+            Console.WriteLine(car1.marka);
+            Console.WriteLine(car2.marka);
+            car2.marka = "Opel";
+            Console.WriteLine(car1.marka);
+            Console.WriteLine(car2.marka);*/
 
+            Console.ReadKey();
         }
-    }
-    class Car
-    {
-        string Model;
     }
 }
